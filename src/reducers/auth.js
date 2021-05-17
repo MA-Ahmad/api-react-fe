966501a834ef9cf38f4c9365731305b12d2eeae7
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-05-17 11:20:46 -0700

    Update auth.js

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,19 +1,18 @@
 const authReducer = (state, { type, payload }) => {
   switch (type) {
     case 'LOGIN': {
-      localStorage.setItem('authToken', JSON.stringify(payload.auth_token));
       localStorage.setItem('authEmail', JSON.stringify(payload.email));
+      localStorage.setItem('isLoggedIn', true);
       return {
         isLoggedIn: true,
-        authToken: payload.auth_token,
         authEmail: payload.email,
       };
     }
     case 'LOGOUT': {
-      localStorage.setItem('authToken', JSON.stringify(null));
+      localStorage.removeItem('isLoggedIn', true);
       localStorage.setItem('authEmail', JSON.stringify(null));
       localStorage.setItem('currentUser', JSON.stringify(null));
-      return { isLoggedIn: false, authToken: null, authEmail: null };
+      return { isLoggedIn: false, authEmail: null };
     }
     default: {
       throw new Error(`Unhandled action type: ${type}`);
