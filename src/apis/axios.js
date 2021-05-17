473c6a1023c045bc966a785412ae143a9f1dba22
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-05-17 11:20:18 -0700

    Update axios.js

--- src/apis/axios.js
+++ src/apis/axios.js
@@ -1,6 +1,4 @@
 import axios from 'axios';
-// import { Toastr } from "neetoui";
-
 axios.defaults.baseURL = '/';
 
 export const setAuthHeaders = (setLoading = () => null) => {
@@ -8,42 +6,8 @@ export const setAuthHeaders = (setLoading = () => null) => {
     Accept: 'applicaion/json',
     'Content-Type': 'application/json',
   };
-  const token = JSON.parse(localStorage.getItem('authToken'));
-  const email = JSON.parse(localStorage.getItem('authEmail'));
-  if (token && email) {
-    axios.defaults.headers['X-Auth-Email'] = email;
-    axios.defaults.headers['X-Auth-Token'] = token;
-  }
+  // axios.defaults.xsrfCookieName = 'CSRF-TOKEN';
+  // axios.defaults.xsrfHeaderName = 'X-CSRF-Token';
+  axios.defaults.withCredentials = true;
   setLoading(false);
-};
-
-export const resetAuthTokens = () => {
-  delete axios.defaults.headers['X-Auth-Email'];
-  delete axios.defaults.headers['X-Auth-Token'];
-};
-
-const handleSuccessResponse = response => {
-  if (response) {
-    response.success = response.status === 200;
-    if (response.data.notice) {
-      // Toastr.success(response.data.notice);
-    }
-  }
-  return response;
-};
-
-const handleErrorResponse = (error, authDispatch) => {
-  if (error.response?.status === 401) {
-    authDispatch({ type: 'LOGOUT' });
-    // Toastr.error(error.response?.data?.error);
-  } else {
-    // Toastr.error(error.response?.data?.error || error.message);
-  }
-  return Promise.reject(error);
-};
-
-export const registerIntercepts = authDispatch => {
-  axios.interceptors.response.use(handleSuccessResponse, error =>
-    handleErrorResponse(error, authDispatch)
-  );
 };
